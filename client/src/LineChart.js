6836678473a1077814646883da6193af02f0882e
Praveen-Kumbhare <132037234+Praveen-Kumbhare@users.noreply.github.com>
2023-07-27 19:38:41 +0530

    graph orientation functionality added

--- client/src/LineChart.js
+++ client/src/LineChart.js
@@ -2,12 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import Chart from "chart.js/auto";
 import { io } from "socket.io-client";
+import "./lineChart.css";
 import zoomPlugin from "chartjs-plugin-zoom";
 
 const LineChart = () => {
   const chartRef = useRef();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [chartType, setChartType] = useState("line");
 
   const chartInstance = useRef(null);
 
@@ -41,13 +43,11 @@ const LineChart = () => {
 
   useEffect(() => {
     if (data.length === 0 || loading) return;
-
     const ctx = chartRef.current.getContext("2d");
-
     const currentYear = new Date().getFullYear();
 
-    const myChart = new Chart(ctx, {
-      type: "line",
+    const config = {
+      type: chartType,
       data: {
         labels: data.map((entry) => {
           const timestamp = new Date(entry?.timestamp);
@@ -65,9 +65,12 @@ const LineChart = () => {
             label: currentYear.toString(),
             data: data.map((entry) => entry?.value),
             borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 2,
-            fill: false,
-            tension: 0.2,
+            borderWidth: 1,
+            fill: chartType === "line",
+            backgroundColor:
+              chartType === "bar"
+                ? "rgba(75, 192, 192, 0.2)"
+                : "rgba(0, 0, 0, 0)",
           },
         ],
       },
@@ -96,13 +99,15 @@ const LineChart = () => {
         },
       },
       plugins: [zoomPlugin],
-    });
+    };
+
+    const myChart = new Chart(ctx, config);
     chartInstance.current = myChart;
 
     return () => {
       myChart.destroy();
     };
-  }, [data, loading]);
+  }, [data, loading, chartType]);
 
   const handleResetZoom = () => {
     if (chartInstance.current) {
@@ -115,9 +120,33 @@ const LineChart = () => {
   }
 
   return (
-    <div>
-      <canvas ref={chartRef} style={{ width: "100%", height: "100px" }} />
-      <button onClick={handleResetZoom}>Reset Zoom</button>
+    <div className="container">
+      <canvas ref={chartRef} style={{ width: "100%", height: "100px" }} />;
+      <div className="btns">
+        <button onClick={handleResetZoom} className="resetButton">
+          Reset Zoom
+        </button>
+        <div className="swCntr">
+          <label className="swLbl">
+            <input
+              type="radio"
+              value="line"
+              checked={chartType === "line"}
+              onChange={() => setChartType("line")}
+            />
+            Line Chart
+          </label>
+          <label className="swlbl">
+            <input
+              type="radio"
+              value="bar"
+              checked={chartType === "bar"}
+              onChange={() => setChartType("bar")}
+            />
+            Bar Chart
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
